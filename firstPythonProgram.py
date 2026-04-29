1df99032552193a331e336b1a61e1e975b5181ab
Gautam Gupta <ggautamg23@gmail.com>
2026-04-29 12:47:34 +0530

    Variables

--- firstPythonProgram.py
+++ firstPythonProgram.py
@@ -1,9 +1,49 @@
 print("Hello World")
 
-print(23+6)
+print(23 + 6)
 
+# Variables
 name = "gautam"
 age = 23
+age2 = age
+print("my name is :", name)
+print("my age is :", age)
+print(age2)
 
-print(name)
-print(age)
+# Operators
+a = 5
+b = 2
+print(a / b)
+
+# Comments
+'''
+c = 5
+d = 3
+'''
+
+# Logical Operators
+
+print(not False)
+print(not (a > b))
+
+
+val1 = True
+val2 = False
+print(val1 and val2)
+
+print(val1 or val2)
+
+# Type Conversion
+
+#Implicit Conversion
+c = 4
+d = 5.65
+print(c + d)
+
+print(type(a))
+
+# Typecasting
+
+e = int("2")
+f = 4.25
+print(e+f)
